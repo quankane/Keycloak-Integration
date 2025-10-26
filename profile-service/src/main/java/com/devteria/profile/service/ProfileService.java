f143f2db187efc776bc77c2cb2a0dfc60ae7a561
quankane <quanducbui2017@gmail.com>
2025-10-27 00:09:04 +0700

    Complete add profile via Keycloak

--- profile-service/src/main/java/com/devteria/profile/service/ProfileService.java
+++ profile-service/src/main/java/com/devteria/profile/service/ProfileService.java
@@ -1,15 +1,22 @@
 package com.devteria.profile.service;
 
+import com.devteria.profile.dto.identity.Credential;
+import com.devteria.profile.dto.identity.TokenExchangeParam;
+import com.devteria.profile.dto.identity.UserCreationParam;
 import com.devteria.profile.dto.request.RegistrationRequest;
 import com.devteria.profile.dto.response.ProfileResponse;
 import com.devteria.profile.mapper.ProfileMapper;
+import com.devteria.profile.repository.IdentityClient;
 import com.devteria.profile.repository.ProfileRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.experimental.NonFinal;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.net.http.HttpHeaders;
 import java.util.List;
 
 @Service
@@ -19,6 +26,15 @@ import java.util.List;
 public class ProfileService {
     ProfileRepository profileRepository;
     ProfileMapper profileMapper;
+    IdentityClient identityClient;
+
+    @Value("${idp.client-id}")
+    @NonFinal
+    String clientId;
+
+    @Value("${idp.client-secret}")
+    @NonFinal
+    String clientSecret;
 
     public List<ProfileResponse> getAllProfiles(){
         var profiles = profileRepository.findAll();
@@ -26,7 +42,38 @@ public class ProfileService {
     }
 
     public ProfileResponse register(RegistrationRequest request){
+        // Create token in Keycloak
+        // Exchange client token
+        var token = identityClient.exchangeToken(TokenExchangeParam.builder()
+                        .grant_type("client_credentials")
+                        .client_id(clientId)
+                        .client_secret(clientSecret)
+                        .scope("openid")
+                .build());
+
+        log.info("token" + token);
+
+        var creationResponse = identityClient.createUser(
+                "Bearer " + token.getAccessToken(),
+                UserCreationParam.builder()
+                        .username(request.getUsername())
+                        .firstName(request.getFirstName())
+                        .lastName(request.getLastName())
+                        .email(request.getEmail())
+                        .enabled(true)
+                        .emailVerified(false)
+                        .credentials(List.of(Credential.builder()
+                                        .type("password")
+                                        .value(request.getPassword())
+                                        .temporary(false)
+                                .build()))
+                .build());
+
+        String headers = creationResponse.getHeaders().get("Location").getFirst();
+        String[] headerList = headers.split("/");
+
         var profile = profileMapper.toProfile(request);
+        profile.setUserId(headerList[headerList.length - 1]);
         profile = profileRepository.save(profile);
 
         return profileMapper.toProfileResponse(profile);
